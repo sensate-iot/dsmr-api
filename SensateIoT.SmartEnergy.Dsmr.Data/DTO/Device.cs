82ebe1ac6f088d414842fb7c4beae0257861edb7
Michel Megens <michel@michelmegens.net>
2021-04-17 11:37:06 +0200

    dto: json: update JSON property names
    Signed-off-by: Michel Megens <michel@michelmegens.net>

--- SensateIoT.SmartEnergy.Dsmr.Data/DTO/Device.cs
+++ SensateIoT.SmartEnergy.Dsmr.Data/DTO/Device.cs
@@ -1,15 +1,23 @@
 ﻿using System;
+using Newtonsoft.Json;
 
 namespace SensateIoT.SmartEnergy.Dsmr.Data.DTO
 {
 	public class Device
 	{
+		[JsonProperty("id")]
 		public int Id { get; set; }
+		[JsonIgnore]
 		public string ServiceName { get; set; }
+		[JsonProperty("name")]
 		public bool Enabled { get; set; }
+		[JsonIgnore]
 		public string PowerSensorId { get; set; }
+		[JsonIgnore]
 		public string GasSensorId { get; set; }
+		[JsonIgnore]
 		public string EnvironmentSensorId { get; set; }
+		[JsonProperty("timestamp")]
 		public DateTime Timestamp { get; set; }
 	}
 }
--- SensateIoT.SmartEnergy.Dsmr.Data/DTO/User.cs
+++ SensateIoT.SmartEnergy.Dsmr.Data/DTO/User.cs
@@ -1,17 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 
+using Newtonsoft.Json;
+
 namespace SensateIoT.SmartEnergy.Dsmr.Data.DTO
 {
 	public class User
 	{
+		[JsonProperty("id")]
 		public Guid Id { get; set; }
+		[JsonProperty("firstName")]
 		public string FirstName { get; set; }
+		[JsonProperty("lastName")]
 		public string LastName { get; set; }
+		[JsonProperty("email")]
 		public string Email { get; set; }
+		[JsonIgnore]
 		public bool Enabled { get; set; }
+		[JsonProperty("timestamp")]
 		public DateTime Timestamp { get; set; }
 
+		[JsonProperty("devices")]
 		public IEnumerable<Device> Devices { get; set; }
 	}
 }
